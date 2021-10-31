887aafc1ebf9284040752808c17cd25ce4a94b04
Muhammad Mustafa <mmustafa.bese20seecs@seecs.edu.pk>
2021-10-31 12:08:08 +0500

    Added User Registration
    -> updated homepage
    -> options for user to register
    -> setup admins dashboard
    
    known issues:
    -> login page referenced but not created yet

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home_view(request):
     return render(request, 'home.html')
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/home")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form":form})
